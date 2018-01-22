482d616f05b3ce08120cfbc28823cdf5ab0479f3
Jonathan Ayala <yojona@msn.com>
2018-01-21 23:29:03 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+/**
+ * Coyota.js
+ * 
+ * Version: 1.0.0
+ * Author: Jonathan Ayala
+ * Created: 01/19/2018
+ * Last update: 01/21/2018
+ */
+
+    OnStart(() =>{
+        screen.create(320, 240)
+    })
+    
+    Always(() =>{
+        // Write your badass game here.
+    })
+
+
+    
