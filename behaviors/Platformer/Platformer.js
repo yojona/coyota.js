e2ca2283d2ec991b078d3cd513e2a27e1d813cc7
Jonathan Ayala <yojona@msn.com>
2018-03-15 09:21:37 -0700

    Update gravity and jump function for platformer behavior

--- behaviors/Platformer/Platformer.js
+++ behaviors/Platformer/Platformer.js
@@ -2,19 +2,39 @@ export default class Platformer {
   constructor () {
     this.inst = null
     this.speed = 0
-    this.maxSpeed = 50
-    this.acceleration = 100
-    this.deceleration = 200
+    this.maxSpeed = 100
+    this.acceleration = 1000
+    this.deceleration = 500
 
     this.direction = null
+
+    this.isMoving = false
+    this.isOnFloor = false
+    this.isJumping = false
+    this.isFalling = false
+
+    this.vectorY = 0
+    this.jumpPower = 320
+    this.fallSpeed = 0
+    this.gravity = 1000
+    this.maxFallSpeed = 1000
   }
 
   moveLeft () {
+    this.isMoving = true
     this.direction = 'left'
   }
 
   moveRight () {
+    this.isMoving = true
     this.direction = 'right'
+  }
+
+  jump () {
+    if (!this.isJumping) {
+      this.vectorY -= this.jumpPower
+      this.isJumping = true
+    }
   }
 
   always () {
@@ -31,8 +51,43 @@ export default class Platformer {
         this.speed = 0
       }
     }
+
+    // Move horizontally
     this.inst.x = this.inst.x + this.speed * dt
 
-    this.direction = null
+    // Reset direction
+    this.direction = ''
+
+
+    // Gravity
+    this.vectorY = this.vectorY < this.maxFallSpeed ? this.vectorY += this.gravity * dt : this.maxFallSpeed
+    this.inst.y += this.vectorY * dt
+    
+    // Ground Collision
+    let potentials = this.inst.collider.potentials()
+
+    for (let wall of potentials) {
+      if (this.inst.collider.collides(wall, CollisionResult) && wall.solid) {
+        this.isMoving = false
+
+        if (CollisionResult.overlap_x < 0 || CollisionResult.overlap_x > 0) {
+          this.speed = 0
+        }
+
+        if (CollisionResult.overlap_y < 0) {
+          this.vectorY = 0
+        }
+
+        if (CollisionResult.overlap_y > 0) {
+          this.vectorY = 0
+          this.isJumping = false
+          this.isOnFloor = true
+          this.vectorY = 0
+        }
+
+        this.inst.x -= CollisionResult.overlap * CollisionResult.overlap_x
+        this.inst.y -= CollisionResult.overlap * CollisionResult.overlap_y
+      }
+    }
   }
 }
