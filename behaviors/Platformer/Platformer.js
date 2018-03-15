49e1e9bd4dca4869e132baf2371b69587f1a043a
Jonathan Ayala <yojona@msn.com>
2018-03-14 18:53:33 -0700

    Fix platformer acceleration

--- behaviors/Platformer/Platformer.js
+++ behaviors/Platformer/Platformer.js
@@ -1,65 +1,104 @@
 export default class Platformer {
   constructor () {
     this.inst = null
-    this.maxSpeed = 3
-    this.jumpStrength = 5
-    this.velX = 0
-    this.velY = 0
-    this.friction = 0.8
-    this.gravity = 0.3
+    this.speed = 0
+    this.maxSpeed = 50
+    this.acceleration = 100
+    this.deceleration = 200
 
-    this.isJumping = false
-    this.isMoving = false
+    this.direction = null
   }
 
   moveLeft () {
-    if (this.velX < this.maxSpeed) {
-      this.isMoving = true
-      this.inst.x -= this.velX
-      this.velX++
-    }
+    this.direction = 'left'
   }
 
   moveRight () {
-    if (this.velX < this.maxSpeed) {
-      this.isMoving = true
-      this.velX++
-      this.inst.x += this.velX
-    }
-  }
-
-  jump () {
-    if (!this.isJumping) {
-      this.isJumping = true
-      this.velY = -this.jumpStrength
-    }
+    this.direction = 'right'
   }
 
   always () {
-    this.velX *= this.friction
-
-    this.inst.y += this.velY
-    this.velY += this.gravity
-
-    let potentials = this.inst.collider.potentials()
-
-    for (let wall of potentials) {
-      if (this.inst.collider.collides(wall, CollisionResult) && wall.solid) {
-        this.isMoving = false
-
-        if (CollisionResult.overlap_x < 0 || CollisionResult.overlap_x > 0) {
-          this.velX = 0
-        }
-
-        if (CollisionResult.overlap_y < 0 || CollisionResult.overlap_y > 0) {
-          this.isJumping = false
-
-          this.velY = 0
-        }
-        this.inst.x -= CollisionResult.overlap * CollisionResult.overlap_x
-        this.inst.y -= CollisionResult.overlap * CollisionResult.overlap_y
+    if (this.direction === 'left' && this.speed > -this.maxSpeed) {
+      this.speed = this.speed - this.acceleration * dt
+    } else if (this.direction === 'right' && this.speed < this.maxSpeed) {
+      this.speed = this.speed + this.acceleration * dt
+    } else {
+      if (this.speed > this.deceleration * dt) {
+        this.speed = this.speed - this.deceleration * dt
+      } else if (this.speed < -this.deceleration * dt) {
+        this.speed = this.speed + this.deceleration * dt
+      } else {
+        this.speed = 0
       }
     }
-    log(this.velY)
+    this.inst.x = this.inst.x + this.speed * dt
+
+    this.direction = null
   }
 }
+
+// export default class Platformer {
+//   constructor () {
+//     this.inst = null
+//     this.maxSpeed = 3
+//     this.jumpStrength = 5
+//     this.velX = 0
+//     this.velY = 0
+//     this.friction = 0.8
+//     this.gravity = 0.3
+
+//     this.isJumping = false
+//     this.isMoving = false
+//   }
+
+//   moveLeft () {
+//     if (this.velX < this.maxSpeed) {
+//       this.isMoving = true
+//       this.inst.x -= this.velX
+//       this.velX++
+//     }
+//   }
+
+//   moveRight () {
+//     if (this.velX < this.maxSpeed) {
+//       this.isMoving = true
+//       this.velX++
+//       this.inst.x += this.velX
+//     }
+//   }
+
+//   jump () {
+//     if (!this.isJumping) {
+//       this.isJumping = true
+//       this.velY = -this.jumpStrength
+//     }
+//   }
+
+//   always () {
+//     this.velX *= this.friction
+
+//     this.inst.y += this.velY
+//     this.velY += this.gravity
+
+//     let potentials = this.inst.collider.potentials()
+
+//     for (let wall of potentials) {
+//       if (this.inst.collider.collides(wall, CollisionResult) && wall.solid) {
+//         this.isMoving = false
+
+//         if (CollisionResult.overlap_x < 0 || CollisionResult.overlap_x > 0) {
+//           this.velX = 0
+//         }
+
+//         if (CollisionResult.overlap_y > 0) {
+//           this.isJumping = false
+//           this.velY = 0
+//         }
+
+//         this.inst.x -= CollisionResult.overlap * CollisionResult.overlap_x
+//         this.inst.y -= CollisionResult.overlap * CollisionResult.overlap_y
+//       }
+//     }
+//     log(this.velY)
+//   }
+// }
