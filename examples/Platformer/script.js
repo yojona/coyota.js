1023942e54c37741bc92bcdbcb237553d8404e2e
Jonathan Ayala <yojona@msn.com>
2018-04-12 20:12:23 -0700

    Add platform methods isMoving, isFalling, isJumping, isOnFloor

--- examples/Platformer/script.js
+++ examples/Platformer/script.js
@@ -13,9 +13,11 @@ import Animation from '../../objects/Sprite/Animation.js'
 import Solid from './../../behaviors/Solid/Solid.js'
 import Keyboard from '../../plugins/Keyboard/Keyboard.js'
 import Platformer from '../../behaviors/Platformer/Platformer.js'
+import Bullet from '../../behaviors/Bullet/Bullet.js'
 
 let resources = {
   playerTexture: './assets/mauricio.png',
+  bulletTexture: './assets/mauricio.png',
   groundTexture: './assets/ground.png'
 }
 
@@ -30,6 +32,7 @@ AssetManager.load(resources).then(() => {
   let ground2
   let ground3
   let player
+  let bullet
   let keyboard = new Keyboard()
 
   OnStart(() => {
@@ -38,38 +41,62 @@ AssetManager.load(resources).then(() => {
     ground2 = new TiledBackground(AssetManager.Textures.groundTexture, 208, 184, 120, 32)
     ground3 = new TiledBackground(AssetManager.Textures.groundTexture, 128, 136, 80, 16)
 
+    bullet = new Sprite(AssetManager.Textures.bulletTexture, 40, 170, 16, 16, 0, 0)
+
     player.addAnimation({
       'Walk': new Animation(player.texture, 1, 3, 10)
     })
 
     player.addBehavior(new Platformer())
 
+    bullet.addBehavior(new Bullet(100, 0, true))
     ground.addBehavior(new Solid())
     ground2.addBehavior(new Solid())
     ground3.addBehavior(new Solid())
   })
 
   Always(() => {
+
+    if (player.behaviors.Platformer.isMoving()) {
+      if (player.behaviors.Platformer.isOnFloor()) {
+        player.setAnimation("Walk").play()
+      }
+      else {
+        player.setAnimation("Walk").stop()
+      }
+    } else  {
+      player.setAnimation("Default")
+    }
+
     if (keyboard.keys[37]) {
       player.behaviors.Platformer.moveLeft()
-      player.setMirrored(true)
-      player.setAnimation('Walk').play()
     }
-    else if (keyboard.keys[39]) {
+    
+    if (keyboard.keys[39]) {
       player.behaviors.Platformer.moveRight()
-      player.setMirrored(false)
-      player.setAnimation('Walk').play()
-    } else {
-      player.setAnimation('Default')
     }
 
     if (keyboard.keys[38]) {
       player.behaviors.Platformer.jump()
     }
 
+    if (player.behaviors.Platformer.vectorX > 0) {
+      player.setMirrored(false)
+    }
+
+    if (player.behaviors.Platformer.vectorX < 0) {
+      player.setMirrored(true)
+    }
+
+    if (keyboard.keys[65]) {
+      let n = Object.assign(bullet)
+      log(n)
+    }
+
     player.draw()
     ground.draw()
     ground2.draw()
     ground3.draw()
+    bullet.draw()
   })
 })
